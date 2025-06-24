1b2cd7379625b84abb6903033aada22ea8cb9223
againstthespread <pdeluca789@gmail.com>
2025-06-24 18:54:20 -0400

    Word Count

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+def word_count():
+    text = get_book_text("books/frankenstein.txt")
+    words = text.split()
+    return len(words)
+
+def main():
+    print(f'{word_count()} words found in the document')
+
+main()
