596fe8a095f8a92a3fd09961f010f9cc33925907
againstthespread <pdeluca789@gmail.com>
2025-06-25 21:37:28 -0400

    Raw Character Count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import word_count
+from stats import character_count
+from stats import get_book_text
 
 def main():
-    print(f'{word_count()} words found in the document')
+    print(character_count(get_book_text("books/frankenstein.txt")))
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,19 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-        return file_contents
+        file_contents2 = file_contents.lower()
+        return file_contents2
 
 def word_count():
     text = get_book_text("books/frankenstein.txt")
     words = text.split()
     return len(words)
+
+def character_count(text):
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
